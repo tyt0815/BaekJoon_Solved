12f2ffb88624364fc69260f9485ea0b097e0877b
tyt0815 <yuy0815@icloud.com>
2023-05-27 21:51:28 +0900

    1074

---
+++ Silver1/bj1074.cpp
@@ -0,0 +1,34 @@
+// 문제: https://www.acmicpc.net/problem/1074
+// 풀이: https://www.notion.so/tyt0815/1074-967d48aac8ac41d2a402fcda3e65c570?pvs=4
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+uint32_t f(uint16_t x, uint16_t y, uint16_t n)
+{
+    if(x == 0 && y == 0){ return 0; }
+    else if(x == 0 && y == 1) { return 1; }
+    else if(x == 1 && y == 0) { return 2; }
+    else if(x == 1 && y == 1) { return 3; }
+
+    uint32_t over = pow(2, n);
+    uint32_t under = over/2;
+    
+    if(x < under && y < under) {return f(x,y, n-1); }
+    else if(x < under && y >= under && y < over){ return f(x, y - under, n-1) + pow(4, n-1); }
+    else if(x >= under && x < over && y < under){ return f(x - under, y, n-1) + pow(4,n-1)*2; }
+    else { return f(x - under, y - under, n-1) + pow(4,n-1)*3; }
+
+}
+
+int main()
+{
+    uint16_t n, x, y;
+    cin >> n >> x >> y;
+
+    cout << f(x, y, n);
+
+    return 0;
+}
