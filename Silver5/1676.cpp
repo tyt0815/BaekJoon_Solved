6c68b860c0552b259bfdf29792ddb6c18658e4c7
김동근 <yuy0815@icloud.com>
2023-07-24 09:45:44 +0900

    1676

--- Silver5/1676.cpp
+++ Silver5/1676.cpp
@@ -2,26 +2,6 @@
 
 using namespace std;
 
-// int main()
-// {
-//     uint64_t n, m = 1, cnt = 0;
-//     cin >> n;
-//     for(int i = 1; i <= n; ++i)
-//     {
-//         m *= i;
-//         while(m % 10 == 0)
-//         {
-//             m/=10;
-//             ++cnt;
-//         }
-        
-//         m %= 10000000000000;
-//     }
-
-//     cout << cnt;
-
-//     return 0;
-// }
 int main()
 {
     uint64_t n, cnt = 0;
