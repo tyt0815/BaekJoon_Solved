a407e1003722760ac3c4244d734f48191c9ebdc5
김동근 <yuy0815@icloud.com>
2023-07-24 10:51:03 +0900

    2751 머지소트 시간초과

--- Silver5/2751.cpp
+++ Silver5/2751.cpp
@@ -1,30 +1,56 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
-
+void sortF(int*, int*, int, int);
 
 int main()
 {
     int n;
-    vector<int> v;
+    int *input, *sorted;
     cin >> n;
+    input = new int[n];
+    sorted = new int[n];
     for(int i = 0; i < n; ++i)
     {
-        int in;
-        cin >> in;
-        v.push_back(in);
+        cin >> input[i];
+        sorted[i] = input[i];
     }
 
-    sort(v.begin(), v.end());
+    sortF(input, sorted, 0, n);
 
-    vector<int>::iterator iter = v.begin();
-    for(; iter != v.end(); ++iter)
+    for(int i = 0; i < n; ++i)
     {
-        cout << *iter << "\n";
+        cout << sorted[i] << '\n';
     }
 
     return 0;
+}
+
+void sortF(int* input, int* sorted, int start, int end)
+{
+    if(start + 1 >= end)
+    {
+        return;
+    }
+    int mid = (start + end) / 2;
+    sortF(input, sorted, start, mid);
+    sortF(input, sorted, mid, end);
+    int i = start, j = mid, k = start;
+    while(i < mid && j < end)
+    {
+        if(input[i] < input[j])
+        {
+            sorted[k] = input[i];
+            ++i; ++k;
+        }
+        else if(input[i] > input[j])
+        {
+            sorted[k] = input[j];
+            ++j; ++k;
+        }
+    }
+    for(; i < mid; ++i,++k){ sorted[k] = input[i]; }
+    for(; j < end; ++j,++k){ sorted[k] = input[j]; }
+    for(i = 0; i < end; ++i){ input[i] = sorted[i]; }
 }
