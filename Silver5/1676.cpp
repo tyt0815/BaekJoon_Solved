54f8bfabde8da73eec054948ba63a39939cd7b51
김동근 <yuy0815@icloud.com>
2023-07-24 09:45:17 +0900

    1676 좀더 깔끔한버전

--- Silver5/1676.cpp
+++ Silver5/1676.cpp
@@ -2,23 +2,39 @@
 
 using namespace std;
 
+// int main()
+// {
+//     uint64_t n, m = 1, cnt = 0;
+//     cin >> n;
+//     for(int i = 1; i <= n; ++i)
+//     {
+//         m *= i;
+//         while(m % 10 == 0)
+//         {
+//             m/=10;
+//             ++cnt;
+//         }
+        
+//         m %= 10000000000000;
+//     }
+
+//     cout << cnt;
+
+//     return 0;
+// }
 int main()
 {
-    uint64_t n, m = 1, cnt = 0;
+    uint64_t n, cnt = 0;
     cin >> n;
     for(int i = 1; i <= n; ++i)
     {
-        m *= i;
-        while(m % 10 == 0)
+        int m = i;
+        while(m % 5 == 0)
         {
-            m/=10;
+            m /= 5;
             ++cnt;
         }
-        
-        m %= 10000000000000;
     }
-
     cout << cnt;
-
     return 0;
 }
