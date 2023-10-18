722a11010550f013b3abc0d34857cc371fbde07a
tyt0815 <yuy0815@icloud.com>
2023-10-18 19:01:09 +0900

    17626

--- Silver3/17626.cpp
+++ Silver3/17626.cpp
@@ -9,14 +9,29 @@ int main()
     cin.tie(0);
 
     uint32_t n;
+    uint32_t *p;
 
     cin >> n;
-    uint32_t m;
-    while(true)
+    p = new uint32_t[n+1];
+    p[1] = 1;
+
+    for(uint32_t i = 2; i < n+1; ++i)
     {
-        m = sqrt(n);
-        n /= m;
+        uint32_t Temp = static_cast<uint32_t>(sqrt(i));
+        if(Temp * Temp == i)
+        {
+            p[i] = 1;
+            continue;
+        }
+        p[i] = 5;
+        for(uint32_t j = 1; j <= i/2; ++j)
+        {
+            p[i] = min(p[i], p[j] + p[i - j]);
+            if(p[i] == 1) { break; }
+        }
     }
+
+    cout << p[n];
 
     return 0;
 }
