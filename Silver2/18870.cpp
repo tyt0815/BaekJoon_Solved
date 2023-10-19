a84fe83e64b9380bc80d2546ed5e61893fb293ef
tyt0815 <yuy0815@icloud.com>
2023-10-19 22:47:50 +0900

    18870 다른버전

--- Silver2/18870.cpp
+++ Silver2/18870.cpp
@@ -1,15 +1,8 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
-
-struct XPos
-{
-    long long Data;
-    uint32_t OriginIndex;
-    uint32_t Pressed;
-};
-
 
 int main()
 {
@@ -17,50 +10,92 @@ int main()
     cin.tie(0);
 
     uint32_t n;
-    XPos *x;
+    vector<long long> OriginX, ProcessedX;
 
     cin >> n;
-    x = new XPos[n];
+    OriginX.resize(n); ProcessedX.resize(n);
 
     for(uint32_t i = 0; i < n; ++i)
     {
-        cin >>x[i].Data;
-        x[i].OriginIndex = i;
-        x[i].Pressed = 0;
-    }
-    
-    sort(x, x+n, [](XPos a, XPos b)
-    {
-        return a.Data < b.Data;
-    }
-    );
-
-    long long Temp =x[0].Data;
-    x[0].Pressed = 0;
-    for(uint32_t i = 1; i < n; ++i)
-    {
-        if(Temp == x[i].Data)
-        {
-            x[i].Pressed = x[i-1].Pressed;
-        }
-        else
-        {
-            Temp = x[i].Data;
-            x[i].Pressed = x[i-1].Pressed + 1;
-        }
+        cin >> OriginX[i];
+        ProcessedX[i] = OriginX[i];
     }
 
-    sort(x, x+n, [](XPos a, XPos b)
-    {
-        return a.OriginIndex < b.OriginIndex;
-    }
-    );
+    sort(ProcessedX.begin(), ProcessedX.end());
+    ProcessedX.erase(unique(ProcessedX.begin(), ProcessedX.end()), ProcessedX.end());
 
     for(uint32_t i = 0; i < n; ++i)
     {
-        cout << x[i].Pressed << ' ';
+        cout << lower_bound(ProcessedX.begin(), ProcessedX.end(), OriginX[i]) - ProcessedX.begin() << ' ';
     }
 
-    delete[] x;
     return 0;
 }
+
+
+// #include <iostream>
+// #include <algorithm>
+
+// using namespace std;
+
+// struct XPos
+// {
+//     long long Data;
+//     uint32_t OriginIndex;
+//     uint32_t Pressed;
+// };
+
+
+// int main()
+// {
+//     ios::sync_with_stdio(false);
+//     cin.tie(0);
+
+//     uint32_t n;
+//     XPos *x;
+
+//     cin >> n;
+//     x = new XPos[n];
+
+//     for(uint32_t i = 0; i < n; ++i)
+//     {
+//         cin >>x[i].Data;
+//         x[i].OriginIndex = i;
+//         x[i].Pressed = 0;
+//     }
+    
+//     sort(x, x+n, [](XPos a, XPos b)
+//     {
+//         return a.Data < b.Data;
+//     }
+//     );
+
+//     long long Temp =x[0].Data;
+//     x[0].Pressed = 0;
+//     for(uint32_t i = 1; i < n; ++i)
+//     {
+//         if(Temp == x[i].Data)
+//         {
+//             x[i].Pressed = x[i-1].Pressed;
+//         }
+//         else
+//         {
+//             Temp = x[i].Data;
+//             x[i].Pressed = x[i-1].Pressed + 1;
+//         }
+//     }
+
+//     sort(x, x+n, [](XPos a, XPos b)
+//     {
+//         return a.OriginIndex < b.OriginIndex;
+//     }
+//     );
+
+//     for(uint32_t i = 0; i < n; ++i)
+//     {
+//         cout << x[i].Pressed << ' ';
+//     }
+
+//     delete[] x;
+//     return 0;
+// }
